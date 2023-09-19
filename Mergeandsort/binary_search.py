345e04942f617ffdcb6b5f9b24ab204a4a32fa9e
Tushar Chawda <tushar.chawda7@gmail.com>
2023-09-19 12:13:56 +0530

    Merge and sort files

---
+++ Mergeandsort/binary_search.py
@@ -0,0 +1,20 @@
+def binary_search( arr ,  target):
+    start, end = 0, len(arr)-1
+    while start <= end:
+        mid = (start+end)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid+1
+    return -1
+
+
+arr = [1,3,4,6,8]
+target = 6
+print(binary_search(arr, target))
+
+
+
+
